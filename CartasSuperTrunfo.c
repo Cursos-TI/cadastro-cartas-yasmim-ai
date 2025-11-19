ebd8669de2a736543c83e3abe8e976ef797cc5a7
yasmim-ai <y.asmim@live.com.pt>
2025-11-19 18:01:34 +0000

    CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,28 +8,32 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   
   //Carta 1
-    char estado1 = 'x';
-    char codigo1[50] = "y";
-    char cidade1[50] = "b";
-    int populacao1 = 4;
-    float area1 = 3;
-    float pib1 =2;
-    int nunpontos1 = 1;
+    char estado1;
+    char codigo1[50];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float PIB1;
+    int nunpontos1;
+    float densipopu1;
+    float PIBperCapita1;
  
   //Carta 2
-    char estado2 = 'c';
-    char codigo2[50] = "a";
-    char cidade2[50] = "d";
-    int populacao2 = 1;
-    float area2 = 2;
-    float pib2 = 3;
-    int nunpontos2 = 4;
+    char estado2;
+    char codigo2[50];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float PIB2;
+    int nunpontos2;
+    float densipopu2;
+    float PIBperCapita2;
 
   // Área para entrada de dados
 
   //Apresentação do jogo
 
-    printf("       Super Trunfo \n");
+    printf("         ---- Super Trunfo ----\n");
 
   //Criação da carta 1
 
@@ -51,14 +55,17 @@ int main() {
     scanf("%f", &area1);
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib1);
+    scanf("%f", &PIB1);
 
     printf("Digite o número de pontos turisticos: \n");
     scanf("%d", &nunpontos1);
 
     printf("\n");
 
-    printf("Dados coletados");
+    printf("   ===== Dados coletados =====");
+
+   densipopu1 = populacao1 / area1;
+   PIBperCapita1 = PIB1 / populacao1;
 
   //Espaço entre a coleta de dados
 
@@ -85,12 +92,15 @@ int main() {
     scanf(" %f", &area2);
 
     printf("Digite o PIB: \n");
-    scanf(" %f", &pib2);
+    scanf(" %f", &PIB2);
 
     printf("Digite o número de pontos turisticos: \n");
     scanf(" %d", &nunpontos2);
 
-    printf("Dados coletados \n");
+    printf("   ===== Dados coletados =====");
+
+    densipopu2 = populacao2 / area2;
+    PIBperCapita2 = PIB2 / populacao2;
 
 
   //Espaço entre coleta de dados e apresentação
@@ -107,8 +117,10 @@ int main() {
     printf("Nome da Cidade: %s\n", cidade1);
     printf("População: %d \n", populacao1);
     printf("Área: %f km²\n", area1);
-    printf("PIB: %f bilhões de reaisv\n", pib1);
+    printf("PIB: %f reais\n", PIB1);
     printf("Número de Pontos Turísticos: %d \n", nunpontos1);
+    printf("Densidade Populacional: %.2f hab/km² \n", densipopu1);
+    printf("PIB per Capita: %.2f reais \n", PIBperCapita1);
   
   //Espaço entre coleta de dados e apresentação
 
@@ -125,8 +137,10 @@ int main() {
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d \n", populacao2);
     printf("Área: %f km²\n", area2);
-    printf("PIB: %f milhões de reias \n", pib2);
+    printf("PIB: %f reias \n", PIB2);
     printf("Número de Pontos Turísticos: %d \n", nunpontos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densipopu2);
+    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);
 
 return 0;
 } 
